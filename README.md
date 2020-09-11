5604a0c9e603aff0599666852eef07bfd2245cda
CompFides <39076812+CompFides@users.noreply.github.com>
2020-09-11 13:07:55 -0400

    Add files via upload
    Version 1.1
    Changed to using a dictionary of regexes for search and substitution

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # deobfuscate
-Converts files obfuscated with multiple layers of encodings, to ascii<br>
-Project also contains a custom Splunk search command that deobfuscates event streams.<br>
+Decodes stdin or file(s) obfuscated with multiple layers of encodings.<br>
+Output file names are appended with '_dbfsctd' to prevent overwriting originals.<br>
+Project has also been ported to a Splunk custom search app.<br>
 
 #### supported encodings
-%XX<br>
-&#xXX<br>
-0xXX<br>
-\\xXX<br>
-&#XX<br>
-char(0xXX)<br>
-chr(XX)<br>
-char(XXX)<br>
-char(XXX,XXX,...)<br>
-\\\\XXX...<br>
+
+encoding | regex
+-------- | ------
+%XX | %([A-F0-9]{2})
+&#xXX | &#x([A-F0-9]{2})(;)
+0xXX | [0&#124;\\\\]x([A-F0-9]{2})
+\\xXX | [0&#124;\\\\]x([A-F0-9]{2}
+&#XX; | &#([0-9]{2,3})(;)
+char(0xXX) | char\(0x([A-F0-9]{2,3})\)
+chr(XXX) | cha?r\(([0-9]{2,3})\)
+char(XXX) | cha?r\(([0-9]{2,3})\)
+char(XXX,XXX,...) | cha?r\(((\d{2,3}),\s(\d{2,3},?\s?)+)\)
+\\\\XXX... | \\\\\\\\([0-7]+)
 
 
 #### options
@@ -20,40 +24,50 @@ char(XXX,XXX,...)<br>
 -p --plus remove plus signs from output<br>
 -i --input path <br>
 -o --output path<br>
+#### examples
+capture stdin and output to stdout<br>
+> cat log.txt | deobfuscate.py
+
+process file.ext and output to stdout<br>
+> deobfuscate.py -i file.ext
+
+process all *.ext files in ./file/location/ and output to ./output/<br>
+> deobfuscate.py -i ./file/location/*.ext -o ./output/<br>
 
 ## Custom Splunk Search Command
 ### Installation
-##### Create an App via the Splunk Web Interface
-Click on "Apps" > "Manage Apps"<br>
-Click on "Create app"<br>
+#### Create an App via the Splunk Web Interface
+Click **Apps** > **Manage Apps**<br>
+Click **Create app**<br>
 
-##### Fill in the following
-Name: deobfuscate<br>
-Folder name: deobfuscate<br>
-Click on "Save"<br>
+#### Configure app
+**Name:** deobfuscate<br>
+**Folder name:** deobfuscate<br>
+**Visible:** **No**<br>
+**Template:** barebones<br>
+Click **Save**<br>
 
-##### Set permissions
-Click on the "Permissions" link for the deobfuscate app <br>
-##### Apply selected role permissions to:
-Select "All apps(system)"<br>
-Click on "Save"<br>
+#### Configure app permissions
+Click **Permissions** link for the deobfuscate app <br>
+Check to ensure **Everyone** role has **"Read"** permissions<br>
+**Apply selected role permissions to:** select **All apps (system)**<br>
+Click **"Save"**<br>
 
-##### Copy deobfuscate.py
-> cp ./deobfuscate_splunk_app/bin/deobfuscate.py $SPLUNK_HOME/etc/apps/deobfuscate/bin/<br>
+#### Copy deobfuscate_splunk.py
+>cp ./deobfuscate/splunk/bin/deobfuscate_splunk.py $SPLUNK_HOME/etc/apps/deobfuscate/bin/<br>
 
-##### Copy commands.conf 
->cp ./deobfuscate_splunk_app/default/commands.conf $SPLUNK_HOME/etc/apps/deobfuscate/default/<br>
+#### Copy commands.conf 
+>cp ./deobfuscate/splunk/default/commands.conf $SPLUNK_HOME/etc/apps/deobfuscate/default/<br>
 
-#### Install splunk-sdk
-##### Move to deobfuscate bin directory
+#### Move to deobfuscate bin directory
 >cd $SPLUNK_HOME/etc/apps/deobfuscate/bin/<br>
 
-##### Install splunk-sdk 
+#### Install splunk-sdk to bin directory
 >pip3 install -t . splunk-sdk
 
 #### Restart Splunk
-Click on "Settings" > "Server controls"<br>
-Click on "Restart Splunk"<br>
+Click **Settings** > **Server controls**<br>
+Click **Restart Splunk**<br>
 
 ### Usage
 Pipe a search to deobfuscate
--- deobfuscate.py
+++ deobfuscate.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
-# deobfuscate v1.0
+# deobfuscate v1.1
 # Copyright 2020 by CompFides
 # MIT License
-# Converts stdin or files, obfuscated with multiple layers of encodings, to ascii
+# Decodes stdin or file(s) obfuscated with multiple layers of encodings
+
+# Change Log v1.1
+# Changed to iterating a dictionary of regexes for searches and substitutions
 
 import argparse
+from collections import OrderedDict
 from glob import glob
 import os
 import re
@@ -12,112 +16,112 @@ import sys
 
 APPEND = '_dbfsctd'
 
-# configure argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument('-l', '--lowercase', action='store_true', help='converts output to all lowercase')
 parser.add_argument('-p', '--plus', action='store_true', help='removes plus signs from output')
-parser.add_argument('-i', '--input', nargs='*', type=str, default=sys.stdin, help='path to files(s) to be deobfuscated')
+parser.add_argument('-i', '--input', nargs='*', type=str, default=sys.stdin, help='path to files(s) to be converted')
 parser.add_argument('-o', '--output', nargs='?', type=str, help='path to output directory')
 args = parser.parse_args()
 
-def urlhex2ascii(matchobj):  # %XX or &#xXX;
-    return chr(int(matchobj.group(1),16))
+def config_regexes():
+    # define dictionary for regex searches
 
-def urldec2ascii(matchobj):  # &#XX
-    return chr(int(matchobj.group(1)))
+    # regexes = OrderedDict([("<regex>",  ('<re.compile flags>' , '<decoder>')), ...])
+    # FYI:  4 escapes = 1 escape
 
-def char0xXX2ascii(matchobj):  # char(0xXX)
-    return chr(int(matchobj.group(1)))
+    regexes = OrderedDict([\
+        ("r'%([A-F0-9]{2})'", ('0', 'hex2ascii')), \
+        ("r'&#x([A-F0-9]{2})(;)'", ('0', 'hex2ascii')), \
+        # keep char(0xXX) before 0xXX, \xXX
+        ("r'char\(0x([A-F0-9]{2,3})\)'", ('re.I', 'hex2ascii')), \
+        ("r'[0|\\\\]x([A-F0-9]{2})'", ('re.I', 'hex2ascii')), \
+        ("r'&#([0-9]{2,3})(;)'", ('re.I', 'dec2ascii')), \
+        ("r'cha?r\(([0-9]{2,3})\)'", ('re.I', 'dec2ascii')), \
+        ("r'cha?r\(((\d{2,3}),\s(\d{2,3},?\s?)+)\)'", ('re.I', 'dec2ascii')), \
+        ("r'\\\\\\\\([0-7]+)'", ('re.I', 'octal2ascii')) \
+        ])
 
-def char2ascii(matchobj):  # chr(XX), char(XXX), char(XXX,XXX,...)
-    ascii = ''
-    # check to for single code or list
-    if not((matchobj.group(1)).find(',')):
-        ascii = chr(int(matchobj.group(1)))
-    else:
-        # extract list of codes
-        thelist = matchobj.group(2).split(",")
-        # loop list and convert
-        for theitem in thelist:
-            ascii += chr(int(theitem))
+    return regexes
+# decoders
+def hex2ascii(matchobj):
+    ascii = chr(int(matchobj.group(1),16))
     return ascii
 
-def octal2ascii(matchobj):  # \\XXX...
-    return ''.join(chr(int(matchobj.group(2)[i:i+2], 8)) for i in range(0, len(matchobj.group(2)), 2))
+def dec2ascii(matchobj):
+    ascii = ''
+    # check for single code or list
+    if ((matchobj.group(1)).find(',')):
+        # extract list of codes
+        list = matchobj.group(1).split(",")
+        # loop list and convert
+        for item in list:
+            ascii += chr(int(item))
+    else:
+        ascii = chr(int(matchobj.group(1)))
+    return ascii
 
-def hex2ascii(matchobj):  # 0xXX, \xXX
-    return ''.join(chr(int(matchobj.group(2)[i:i+2], 16)) for i in range(0, len(matchobj.group(2)), 2))
+def octal2ascii(matchobj):
+    ascii = ''.join(chr(int(matchobj.group(1)[i:i+2], 8)) for i in range(0, len(matchobj.group(1)), 2))
+    return ascii
 
-def deobfuscate(the_line):
-    # searches line for regex patterns and replaces matches with ascii
-    # the_line - the line of text being processed
-
-    # declare variables for regex searches
-    urlhex = re.compile(r'%([a-f0-9]{2,2})', flags=re.I) # %XX
-    urlhex2 = re.compile(r'&#x([a-f0-9]{2,2})(;)', flags=re.I) # &#xXX;
-    hexstr = re.compile(r'([0|\\]x)([a-f0-9]+)', flags=re.I)  # 0xXX..., \xXX...
-    urldec = re.compile(r'&#([0-9]{2,3})', flags=re.I) # &#XX
-    char0xXX = re.compile(r'\bchar\(0x([a-f0-9]{2,3})\)', flags=re.I) # char(0xXX)
-    charXXX = re.compile(r'\b(cha?r)\(([0-9]{2,3})(,\s*\d+)*\)', flags=re.I) # chr(XX), char(XXX), char(XX,XXX,...)
-    octal = re.compile(r'\\\\([0-7]+)', flags=re.I) # \\XXX...
+def dispatch_decoder(line, regexes):
+    # searches line for regex patterns and sends matches to appropriate decoder function
+    # line - the line being processed
 
     obfuscated = True
+    not_found = 0
     while obfuscated:
-        if urlhex.search(the_line):
-            the_line = urlhex.sub(urlhex2ascii, the_line)
-        elif urlhex2.search(the_line):
-            the_line = urlhex2.sub(urlhex2ascii, the_line)
-        elif hexstr.search(the_line):
-            the_line = hexstr.sub(hex2ascii, the_line)
-        elif urldec.search(the_line):
-            the_line = urldec.sub(urldec2ascii, the_line)
-        elif char0xXX.search(the_line):
-            the_line = char0xXX.sub(char0xXX2ascii, the_line)
-        elif charXXX.search(the_line):
-            the_line = charXXX.sub(char2ascii, the_line)
-        elif octal.search(the_line):
-            the_line = octal.sub(octal2ascii, the_line)
-        else:
+        for regex, config in regexes.items():
+            if re.compile(eval(regex), flags=eval(config[0])).search(line):
+                line = re.compile(eval(regex), flags=eval(config[0])).sub(eval(config[1]), line)
+            else:
+                not_found += 1
+        # check whether to exit
+        if not_found == len(regexes.keys()):
             obfuscated = False
-        # remove plus signs
-        if args.plus:
-            the_line = re.sub(r'\+', '', the_line)
-        # convert to lower case
-        if args.lowercase:
-            the_line = the_line.lower()
-    return(the_line)
+        # reset not_found
+        else:
+            not_found = 0
+    # remove plus signs
+    if args.plus:
+        line = re.sub(r'\+', ' ', line)
+    # convert to lower case
+    if args.lowercase:
+        line = line.lower()
+    return(line)
 
 def main():
 
-    # test for sys.stdin
+    # create dictionary of regexes
+    regexes = config_regexes()
+
+    # sys.stdin
     if not sys.stdin.isatty():
         input_line = str(sys.stdin.readlines())[2:-4]
-        output_line = deobfuscate(input_line)
+        output_line = dispatch_decoder(input_line, regexes)
         sys.stdout.write(output_line)
 
-    # file(s) | directory | wildcard
+    # file(s) or wildcards
     elif args.input:
         input_file_names = []
         for arg in args.input:
-            # test if arg is a directory
+            # test for directory
             if os.path.isdir(arg):
                 arg = os.path.join(os.path.dirname(os.path.realpath(__file__)), arg, '*')
             input_file_names += glob(arg)
         # iterate file names
         for input_file_name in input_file_names:
             input_file = open(input_file_name,"r")
-            # construct output file name
+            # construct path and output file name
             if args.output:
                 f_name, f_ext = os.path.splitext(input_file_name)
                 output_file_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), args.output, f_name + APPEND + f_ext)
-                # test if directory exists
                 if not os.path.exists(os.path.dirname(output_file_name)):
-                    # create directory
                     os.makedirs(os.path.dirname(output_file_name))
                 output_file = open(output_file_name, "w")
             # iterate file lines
             for input_line in input_file:
-                output_line = deobfuscate(input_line)
+                output_line = dispatch_decoder(input_line, regexes)
                 # output to destination
                 if args.output:
                     output_file.write(output_line)
